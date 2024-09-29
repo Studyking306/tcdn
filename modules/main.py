c02870b3b417142d8095d54b5afb853cf7bd9490
hectorog612 <153538709+hectorog612@users.noreply.github.com>
2024-09-29 19:42:56 +0530

    Update main.py

--- modules/main.py
+++ modules/main.py
@@ -40,13 +40,13 @@ bot = Client("bot",
 # Get the MongoDB collection for this bot
 collection = get_collection(BOT_NAME, MONGO_URI)
 # Constants
-OWNER_IDS = [7078486618]  # Replace with the actual owner user IDs
+OWNER_IDS = [7079041090]  # Replace with the actual owner user IDs
 
 # Global variables
-log_channel_id = 7078486618
+log_channel_id = -1002470919436
 authorized_users = []
 ALLOWED_CHANNEL_IDS = []
-my_name = "𝐊𝐔𝐍𝐀𝐋❤️"
+my_name = "𝗛𝗲𝗰𝘁𝗼𝗿𝗢𝗴"
 overlay = None 
 accept_logs = 0
 bot_running = False
