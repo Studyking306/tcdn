c5a03bfc6a3f4e30650e18eae22e7b1a8c3adde5
Studyking306 <akbartheking7860@gmail.com>
2024-10-31 01:33:19 +0530

    Update config.py

--- modules/config.py
+++ modules/config.py
@@ -3,4 +3,4 @@ BOT_NAME = "gillidanda"
 BOT_TOKEN = "7131670643:AAEuQKLZjZ9MTgAC-lPE_XD1o5sfrx6JjYI"
 API_ID = 22188044
 API_HASH = "099e3a1dce52b7677299c3ab8ab3b6ca"
-MONGO_URI = "mongodb+srv://mrnobody:modernhackers@mrnobody.q8e87ij.mongodb.net/?retryWrites=true&w=majority&appName=MrNobody"
+MONGO_URI = "mongodb+srv://akbarakbar4329:sFqcKBFqzsYobYfu@cluster0.keqgcg0.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
